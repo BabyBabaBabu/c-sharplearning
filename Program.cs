bbfae6b4df3462c3b649b3ce07ad8e4910d42670
BabyBabaBabu <iamkitaaresort@gmail.com>
2023-07-27 17:13:50 +0300

    Added Hello World Code

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, Me!");
+
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        // This line prints the famous "Hello World"
+        Console.WriteLine("Hello World!");
+    }
+}
+
