a8953b90613e5430c3bbb13ffb53465b17fea53f
BabyBabaBabu <iamkitaaresort@gmail.com>
2023-07-29 10:30:20 +0300

    Added Hello World File with the Code.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, Me!");
 
-using System;
-
-class HelloWorld
-{
-    static void Main()
-    {
-        // This line prints the famous "Hello World"
-        Console.WriteLine("Hello World!");
-    }
-}
-
---
+++ hello-world.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld
+{
+class HelloWorld
+{
+    static void Main()
+    {
+        // This line prints the famous "Hello World"
+        Console.WriteLine("Hello World!");
+    }
+}
+
+}
